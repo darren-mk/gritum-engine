97adaee0ef43d604282dfeb434cf011068110443
Darren Kim <darren.minsoo.kim@gmail.com>
2025-12-16 08:52:22 -0500

    add giraffe for web server

--- Program.fs
+++ Program.fs
@@ -1,2 +1,18 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.Hosting
+open Giraffe
+
+let webApp =
+    choose [
+        route "/health" >=> text "ok"
+        route "/" >=> text "gritum"
+        setStatusCode 404 >=> text "not found" ]
+
+[<EntryPoint>]
+let main args =
+    let builder = WebApplication.CreateBuilder(args)
+    let app = builder.Build()
+    app.UseGiraffe(webApp)
+    app.Run("http://127.0.0.1:3000")
+    0
+
