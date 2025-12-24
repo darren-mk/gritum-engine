ca13295f880666a6e094e45bd52099e05e864dbd
Darren Kim <darren.minsoo.kim@gmail.com>
2025-12-24 01:45:16 -0500

    parse and normalize fees from xml

--- src/gritum/core.clj
+++ src/gritum/core.clj
@@ -1,1 +1,90 @@
 (ns gritum.core)
+
+(defn jump-map [xml k]
+  (when (= k (:tag xml)) (:content xml)))
+
+(defn jump-vec [xml k]
+  (let [picked (filter #(= k (:tag %)) xml)]
+    (if (= 1 (count picked))
+       (:content (first picked))
+       (throw (ex-info "multiple nodes with same tag"
+                       {:key k
+                        :xml xml})))))
+
+(defn jump [xml k]
+  (cond (map? xml) (jump-map xml k)
+        (sequential? xml) (jump-vec xml k)
+        :else (throw (ex-info "unknown structure in xml" {}))))
+
+(defn traverse [xml ks]
+  (reduce jump xml ks))
+
+(def loan-path
+  [:MESSAGE :DOCUMENT_SETS
+   :DOCUMENT_SET :DOCUMENTS
+   :DOCUMENT :DEAL_SETS :DEAL_SET
+   :DEALS :DEAL :LOANS :LOAN])
+
+(def zero-tolerance-fee-types
+  #{"AppraisalFee" "CreditReportFee"
+    "FloodDeterminationFee" 
+    "AppraisalFieldReviewFee"
+    "TaxServiceFee"})
+
+(defn ->tolerance [section fee-type]
+  (case section
+    ("OriginationCharges" "ServicesYouCannotShopFor") 0.0
+    ("ServicesYouCanShopFor") 0.1
+    ("ServicesBorrowerDidNotShopFor") (if (zero-tolerance-fee-types fee-type) 0.0 0.1)
+    ("TaxesAndOtherGovernmentFees") (if (= fee-type "RecordingFee") 0.1 0.0)
+    ("Prepaids" "InitialEscrowPaymentAtClosing"
+     "ServicesBorrowerDidShopFor" "OtherCosts") nil))
+
+(defmulti normalize
+  (fn [_ k] k))
+
+(defmethod normalize :fee [xml _]
+  (let [detail (traverse xml [:FEE :FEE_DETAIL])
+        fee-content (:content xml)
+        payments-node (first (filter #(= :FEE_PAYMENTS (:tag %)) fee-content))
+        payments (when payments-node
+                   (filter #(= :FEE_PAYMENT (:tag %)) (:content payments-node)))
+        section (->> [:IntegratedDisclosureSectionType] (traverse detail) first)
+        fee-type (->> [:EXTENSION :OTHER :FEE_DETAIL_EXTENSION :FeeItemType]
+                      (traverse detail) first)]
+    #:fee{:type fee-type :section section
+          :tolerance (->tolerance section fee-type)
+          :amount (if (seq payments)
+                    (->> payments
+                         (filter #(= "Buyer" (first (traverse % [:FEE_PAYMENT :FeePaymentPaidByType]))))
+                         (map #(->> [:FEE_PAYMENT :FeeActualPaymentAmount] (traverse %) first parse-double))
+                         (reduce + 0.0))
+                    0.0)
+          :paid-by "Buyer"}))
+
+(defn ->domain [xml]
+  (let [loan (traverse xml loan-path)
+        fees (traverse loan [:FEE_INFORMATION :FEES])]
+    {:fees (mapv #(normalize % :fee) fees)}))
+
+(defn all-unique? [coll]
+  (= (count coll)
+     (count (distinct coll))))
+
+(defn xml->edn
+  "connect by tag and content, ignore attrs"
+  [xml]
+  (prn xml)
+  (cond (nil? xml) nil
+        (map? xml) {(:tag xml) (xml->edn (:content xml))}
+        (seq? xml) (xml->edn (apply vector xml))
+        (vector? xml) (cond (= 1 (count xml))
+                            (let [fst (first xml)
+                                  calc (xml->edn (first xml))]
+                              (if (:tag fst) [calc] calc))
+                            (all-unique? (map :tag xml))
+                            (reduce (fn [a {:keys [tag content]}]
+                                      (assoc a tag (xml->edn content))) {} xml)
+                            :else (mapv xml->edn xml))
+        (contains? #{"true" "false"} xml) (read-string xml)
+        :else xml))
--- test/gritum/core_test.clj
+++ test/gritum/core_test.clj
@@ -1,6 +1,156 @@
 (ns gritum.core-test
   (:require
-   [clojure.test :as t]))
+   [clojure.edn :as edn]
+   [clojure.data.xml :as xml]
+   [clojure.java.io :as io]
+   [clojure.test :as t]
+   [gritum.core :as sut]))
 
-(t/deftest empty-test
-  (t/is true))
+(defn load-edn [filename]
+   (if-let [resource-url (io/resource filename)]
+     (-> resource-url slurp edn/read-string)
+     (throw (Exception. (str "Resource not found: " filename)))))
+
+(defn load-xml [filename]
+  (if-let [resource-url (io/resource filename)]
+    (-> resource-url slurp xml/parse-str)
+    (throw (Exception. (str "Resource not found: " filename)))))
+
+(t/deftest jump-map-test
+  (let [sample {:tag :A :content ["val"]}]
+    (t/testing "returns content when tag matches"
+      (t/is (= ["val"] (sut/jump-map sample :A))))
+    (t/testing "returns nil when tag does not match"
+      (t/is (nil? (sut/jump-map sample :B))))))
+
+(t/deftest jump-vec-test
+  (let [sample [{:tag :A :content [1]}
+                {:tag :B :content [2]}]]
+    (t/testing "returns content when exactly one matching tag exists in the list"
+      (t/is (= [1] (sut/jump-vec sample :A))))
+    (t/testing "throws exception if tag is missing or duplicated"
+      (t/is (thrown? clojure.lang.ExceptionInfo (sut/jump-vec sample :C)))
+      (let [dups [{:tag :A} {:tag :A}]]
+        (t/is (thrown? clojure.lang.ExceptionInfo (sut/jump-vec dups :A)))))))
+
+(t/deftest jump-test
+  (t/testing "dispatches to appropriate logic based on input type (Map/Vector)"
+    (t/is (= ["val"] (sut/jump {:tag :root :content ["val"]} :root)))
+    (t/is (= ["val"] (sut/jump [{:tag :child :content ["val"]}] :child)))))
+
+(t/deftest traverse-test
+  (let [sample {:tag :root
+                :content [{:tag :mid
+                           :content [{:tag :leaf :content ["END"]}]}]}]
+    (t/testing "traverses deep down the path and returns the final content"
+      (t/is (= ["END"]
+               (sut/traverse sample [:root :mid :leaf]))))))
+
+(def sample-fee-xml
+  {:tag :FEE :attrs {}
+   :content
+   [{:tag :FEE_DETAIL :attrs {}
+     :content
+     [{:tag :FeePaidToType :attrs {} :content ["Lender"]}
+      {:tag :FeeTotalPercent :attrs {} :content ["0.2500"]}
+      {:tag :IntegratedDisclosureSectionType
+       :attrs {} :content ["OriginationCharges"]}
+      {:tag :RegulationZPointsAndFeesIndicator
+       :attrs {} :content ["true"]}
+      {:tag :EXTENSION :attrs {}
+       :content
+       [{:tag :OTHER :attrs {}
+         :content [{:tag :FEE_DETAIL_EXTENSION :attrs {}
+                    :content
+                    [{:tag :FeeItemType :attrs {}
+                      :content ["LoanDiscountPoints"]}]}]}]}]}
+    {:tag :FEE_PAYMENTS :attrs {}
+     :content
+     [{:tag :FEE_PAYMENT :attrs {}
+       :content
+       [{:tag :FeeActualPaymentAmount :attrs {} :content ["360.00"]}
+        {:tag :FeePaymentPaidByType :attrs {} :content ["Buyer"]}
+        {:tag :FeePaymentPaidOutsideOfClosingIndicator :attrs {}
+         :content ["false"]}]}]}]})
+
+(t/deftest normalize-test
+  (t/testing "fee"
+    (t/is (= #:fee{:type "LoanDiscountPoints",
+                   :amount 360.0,
+                   :tolerance 0.0
+                   :section "OriginationCharges",
+                   :paid-by "Buyer"}
+             (sut/normalize sample-fee-xml :fee)))))
+
+(t/deftest xml->edn-test
+  (t/testing "unwrap sequence"
+    (let [single-xml [{:tag :FEE :content ["FeeA"]}]
+          multi-xml  [{:tag :FEE :content ["FeeA"]}
+                      {:tag :FEE :content ["FeeB"]}]]
+      (t/testing "vector with single item returns a vector"
+        (let [result (sut/xml->edn single-xml)]
+          (t/is (vector? result))
+          (t/is (= [{:FEE "FeeA"}] result))))
+      (t/testing "vector with multiple item returns a vector"
+        (let [result (sut/xml->edn multi-xml)]
+          (t/is (vector? result))
+          (t/is (= [{:FEE "FeeA"} {:FEE "FeeB"}] result))))))
+  (t/testing "1. Leaf Node Handling (Values)"
+    (t/testing "Strings are unwrapped from vector"
+      (let [input ["Lender"]]
+        (t/is (= "Lender" (sut/xml->edn input)))))
+    (t/testing "Booleans are parsed and unwrapped"
+      (t/is (= true (sut/xml->edn ["true"])))
+      (t/is (= false (sut/xml->edn ["false"])))))
+  (t/testing "2. Single Item Consistency (The Main Fix)"
+    (t/testing "Single structural node is KEPT as a Vector (unlike before)"
+      (let [input [{:tag :LOAN :content ["Data"]}]]
+        ;; Your new logic: It has a :tag, so it stays in a vector.
+        (t/is (= [{:LOAN "Data"}] (sut/xml->edn input)))
+        (t/is (vector? (sut/xml->edn input)))))
+    (t/testing "Single leaf node is UNWRAPPED"
+      (let [input [{:tag :Amount :content ["100"]}]]
+        ;; Inside Amount, content is ["100"].
+        ;; "100" has no tag, so it unwrap to "100".
+        ;; Result: {:Amount "100"} wrapped in vector?
+        ;; No, wait. xml->edn on the ROOT returns the vector.
+        (t/is (= [{:Amount "100"}] (sut/xml->edn input))))))
+  (t/testing "3. Multiple Items Handling"
+    (t/testing "Multiple same-tag nodes remain a Vector"
+      (let [input [{:tag :FEE :content ["A"]}
+                   {:tag :FEE :content ["B"]}]]
+        (t/is (= [{:FEE "A"} {:FEE "B"}] (sut/xml->edn input)))))
+    (t/testing "Multiple unique-tag nodes become a Map (siblings)"
+      (let [input [{:tag :Name :content ["John"]}
+                   {:tag :Age  :content ["30"]}]]
+        ;; (all-unique?) branch triggers here
+        (t/is (= {:Name "John", :Age "30"} (sut/xml->edn input))))))
+  (t/testing "4. Mixed/Nested Integration Test (FEE Data)"
+    ;; This tests the behavior of nested children
+    (let [input {:tag :FEE
+                 :content [{:tag :FEE_DETAIL
+                            :content [{:tag :Type :content ["Origination"]}]}]}]
+      ;; Logic Trace:
+      ;; 1. Root :FEE -> Map Key
+      ;; 2. Content is [{:tag :FEE_DETAIL...}] (Count 1)
+      ;; 3. Item has :tag, so it becomes a VECTOR: [{:FEE_DETAIL ...}]
+      ;; 4. Inside FEE_DETAIL, Content is [{:tag :Type...}] (Count 1)
+      ;; 5. Item has :tag, so it becomes a VECTOR: [{:Type ...}]
+      ;; 6. Inside Type, Content is ["Origination"] (Count 1)
+      ;; 7. Item is String (no tag), so it UNWRAPS: "Origination"
+      (t/is (= {:FEE [{:FEE_DETAIL [{:Type "Origination"}]}]}
+               (sut/xml->edn input)))
+      ;; Note for Developer:
+      ;; Because of the new logic, even unique children like :FEE_DETAIL
+      ;; are now returned as Vectors of 1 item, not Maps.
+      (t/is (vector?
+             (-> [{:tag :Container :content
+                   [{:tag :Child :content ["Val"]}]}]
+                 sut/xml->edn first :Container))))))
+#_
+(t/deftest domain-test
+  (t/testing "purchase fixed ucd 2.0"
+    (let [folder-base "data/UCD v2.0 Sample XML Files"
+          filename "Purchase Fixed UCD v2.0."]
+      (t/is (= (load-edn (str folder-base " Extraction/" filename "edn"))
+               (->> (str folder-base "/" filename "xml") load-xml sut/->domain))))))
